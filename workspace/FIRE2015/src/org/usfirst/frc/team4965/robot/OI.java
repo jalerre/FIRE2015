cd1a0a19b38da67c58b4e0f7b00d95bf3025f5a0
Aerdegath <msbell@clemson.edu>
2015-01-25 21:36:52 +0000

    Test Nitrous.io commit

---
+++ workspace/FIRE2015/src/org/usfirst/frc/team4965/robot/OI.java
@@ -0,0 +1,51 @@
+package org.usfirst.frc.team4965.robot;
+
+import edu.wpi.first.wpilibj.buttons.Button;
+import org.usfirst.frc.team4965.robot.commands.ExampleCommand;
+import edu.wpi.first.wpilibj.Joystick;
+//test difference
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+	Joystick controller = new Joystick(0);
+    //Joystick controllerTwo = new Joystick(2);
+    
+    public double leftStickY()
+    {
+        if (controller.getY() < 0.2 && controller.getY() > -0.2)
+        {
+            return 0.0;
+        }
+        return controller.getY();
+    }
+    public double rightStickY()
+    {
+        if (controller.getRawAxis(5) < 0.2 && controller.getRawAxis(5) > -0.2)
+        {
+            return 0.0;
+        }
+        return controller.getRawAxis(5);
+    }
+    
+    public double leftStickX()
+    {
+        if (controller.getX() < 0.2 && controller.getX() > -0.2)
+        {
+            return 0.0;
+        }
+        return controller.getX();
+    }
+    
+    public double Twist()
+    {
+        if (controller.getRawAxis(3) < 0.3 && controller.getRawAxis(3) > -0.3)
+        {
+            return 0.0;
+        }
+        
+        return controller.getRawAxis(3);
+    }
+}
+
